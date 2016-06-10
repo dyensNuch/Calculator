8aa2ee7534f80a221c12a79fd61ca823948d0cb3
dyensNuch <dyens@hotmail.com>
2016-06-10 11:12:57 +0700

    create something method

--- app/src/test/java/com/example/nuch_nb/calculator_unittest/orangefile.java
+++ app/src/test/java/com/example/nuch_nb/calculator_unittest/orangefile.java
@@ -4,4 +4,8 @@ package com.example.nuch_nb.calculator_unittest;
  * Created by Nuch-NB on 10/6/2559.
  */
 public class orangefile {
+    public String getSomething()
+    {
+        return "someting";
+    }
 }
